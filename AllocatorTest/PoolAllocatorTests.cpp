e8cb691d06ee9112cb7eaf8ab3cc4d913d6840e7
Tleety <dahltobbe@hotmail.com>
2016-09-15 15:47:19 +0200

    Started on iterator class

--- AllocatorTest/PoolAllocatorTests.cpp
+++ AllocatorTest/PoolAllocatorTests.cpp
@@ -16,16 +16,27 @@ namespace AllocatorTest
 	//Class to test all the different use cases for our custom memory pool allocator.
 	TEST_CLASS(PoolAllocatorTests)
 	{
-		//Test a simple case of allocating memory.
+		//Test a simple case of allocating a small amount of memory 
+		//and then adding data to it.
 		TEST_METHOD(POOL_Allocation)
 		{
-			//Allocate memory with the size of 50 ints.
-			PoolAllocator<int> pool(50);
+			int allocationVolume = 50;
+			PoolAllocator<int> pool(allocationVolume);
 
-			for (int i = 0; i < 50; i++) {
+			for (int i = 0; i < allocationVolume; i++) {
 				pool.Allocate(i);
 			}
-			
+		}
+
+		//Test a case of allocating large volumes of memory.
+		TEST_METHOD(POOL_AllocationVolume)
+		{
+			int allocationVolume = 100'000'000;
+			PoolAllocator<int> pool(allocationVolume);
+
+			for (int i = 0; i < allocationVolume) {
+				pool.Allocate(i);
+			}
 		}
 
 		//Test a simple case of freeing memory.
