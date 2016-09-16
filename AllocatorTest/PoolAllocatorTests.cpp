0e289742bdcd52d54d142da31b021134ba713460
sippeangelo <holmberg.simon@gmail.com>
2016-09-16 16:26:00 +0200

    Tests rewritten for GTest + Linux compability

--- AllocatorTest/PoolAllocatorTests.cpp
+++ AllocatorTest/PoolAllocatorTests.cpp
@@ -1,62 +1,50 @@
-#include "stdafx.h"
-#include <CppUnitTest.h>
 #include <thread>
 #include <vector>
+
+#include <gtest/gtest.h>
+
 #include "../Allocator/PoolAllocator.h"
 
-
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-//Undefine to use normal memory allocator
+// Undefine to use normal memory allocator
 #define POOL_ALLOCATOR
 
-namespace AllocatorTest
+// Test a simple case of allocating memory.
+TEST(PoolAllocator, Allocation)
 {
+    // Allocate memory with the size of 50 ints.
+    PoolAllocator<int> pool(50);
 
-	//Class to test all the different use cases for our custom memory pool allocator.
-	TEST_CLASS(PoolAllocatorTests)
-	{
-		//Test a simple case of allocating memory.
-		TEST_METHOD(POOL_Allocation)
-		{
-			//Allocate memory with the size of 50 ints.
-			PoolAllocator<int> pool(50);
-
-			for (int i = 0; i < 50; i++) {
-				pool.Allocate(i);
-			}
-			
-		}
-
-		//Test a simple case of freeing memory.
-		TEST_METHOD(POOL_Free)
-		{
-
-		}
-
-		//Test several continuous allocations and free's to see if the
-		//memory stays intact without errors or diskfragmentation.
-		TEST_METHOD(POOL_ContinuousAllocationAndFree)
-		{
-
-		}
-
-		//Test using more memory than we have allocated and see that it is 
-		//handled correctly.
-		TEST_METHOD(POOL_Boundries)
-		{
-
-		}
-
-		//Check the available space left in the memory pool
-		//to make sure it's calculated correctly.
-		TEST_METHOD(POOL_AvailableSpace)
-		{
-
-		}
-	};
+    for (int i = 0; i < 50; i++) {
+        pool.Allocate(i);
+    }
 }
 
+// Test a simple case of freeing memory.
+TEST(PoolAllocator, Free)
+{
+
+}
+
+// Test several continuous allocations and free's to see if the
+// memory stays intact without errors or diskfragmentation.
+TEST(PoolAllocator, ContinuousAllocationAndFree)
+{
+
+}
+
+// Test using more memory than we have allocated and see that it is
+// handled correctly.
+TEST(PoolAllocator, Boundaries)
+{
+
+}
+
+// Check the available space left in the memory pool
+// to make sure it's calculated correctly.
+TEST(PoolAllocator, AvailableSpace)
+{
+
+}
 
 
 
