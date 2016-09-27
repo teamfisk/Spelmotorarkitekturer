94e00001f15146859d85b5d09666f876b1b71c1f
ViktorLjung <ljunga_93@hotmail.com>
2016-09-27 13:06:34 +0200

    Removed test crash code in main

--- Allocator/Main.cpp
+++ Allocator/Main.cpp
@@ -7,17 +7,6 @@
 int main()
 {	
 
-    std::vector<int> coolVector = { 0, 1, 4, 5, 2 };
-
-    auto it1 = coolVector.begin();
-    
-
-    auto it2 = coolVector.begin();
-    it2++;
-
-    coolVector.insert(it1, 7);
-    (*it2);
-
 
 	return 0;
 }
