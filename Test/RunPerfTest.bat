90780a220ef01a54581f53fc01420f2727f741c2
Martin Lundgren <woozie663@gmail.com>
2016-09-27 16:51:36 +0200

    Tried to add a loop to RunPerfTest.bat
    Batch files suck. Worst language ever.

--- Test/RunPerfTest.bat
+++ Test/RunPerfTest.bat
@@ -1,7 +1,19 @@
 @echo off
-REM How to use 	 	
-REM ./RunPerfTest.bat MALLOC_SMallAllocationComparison
-REM					  ^ the name of the test to run. Run ONE test at a time!
-"../bin/Test.exe" %1 -o %1.xml -d "yes" -r xml
-py .\CatchXMLFileToGNUPlotData.py "%1.xml" "%1.txt"
+REM How to use 	 										↓ number of times to run each test.
+REM ./RunPerfTest.bat MALLOC_SMallAllocationComparison 10
+REM					  ^ the name of the test(s) to run. It's possible to run multiple tests. Each test result will be exported into a seperate txt file.
+
+IF NOT "%2"=="" THEN (
+	SET arg2=1
+) ELSE (
+	SET arg2=%2
+)
+
+echo on
+
+FOR %%A IN (1, 1, %arg2%) DO (
+	ECHO %%A
+	REM "../bin/Test.exe" %1 -o %1.xml -d "yes" -r xml
+	REM py .\CatchXMLFileToGNUPlotData.py "%1.xml"
+)
 
