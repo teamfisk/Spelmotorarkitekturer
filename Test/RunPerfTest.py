1b55666daf5cd614c8033307e72f2d3f6bcdf5df
Martin Lundgren <woozie663@gmail.com>
2016-09-27 17:26:19 +0200

    Added loop to RunPerfTest
    Can now run test and export test results multiple times from the cmd
    line.

--- Test/RunPerfTest.py
+++ Test/RunPerfTest.py
@@ -4,19 +4,23 @@ from CatchXMLFileToGNUPlotData import CatchXMLFileToGNUPlotData
 
 # Used when running this script directly form a console
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: arg1\n")
-        print("arg1 is the path to the XML file created by catch.")
-        input()
-        sys.exit(0)
-    else:
-        #arg1 = "[StackAllocator]"
+    if len(sys.argv) == 2 or len(sys.argv) == 3:
+        testruns = 1
+        if len(sys.argv) == 3:
+            testruns = int(sys.argv[2])
+
         arg1 = sys.argv[1]
         args = ["../bin/Test.exe", arg1, '-d', 'yes', "-r", "xml", "-o", arg1 + ".xml"]
+        #arg1 = "[StackAllocator]"
+        for i in range(testruns):
+            p = subprocess.Popen(args)
+            p.wait()
 
-        p = subprocess.Popen(args)
-        p.wait()
-
-        CatchXMLFileToGNUPlotData(arg1 + ".xml")
-
-        input("Program done. Press any key to continue.")
+            CatchXMLFileToGNUPlotData(arg1 + ".xml")
+        #input("Program done. Press any key to continue.")
+    else:
+        print(len(sys.argv))
+        print("Usage:")
+        print("arg1 is the test(s) to run.")
+        print("arg2 (optional) is the number of times to run the test.")
+        sys.exit(0)
