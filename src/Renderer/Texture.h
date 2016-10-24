29dd046ba957de0b467005b9a43931305e4c5b7c
Martin Lundgren <woozie663@gmail.com>
2016-10-24 16:51:14 +0200

    Can load opengl texture

--- src/Renderer/Texture.h
+++ src/Renderer/Texture.h
@@ -16,12 +16,12 @@ protected:
 public:
 	~Texture();
 
-	void Bind(GLenum textureUnit = GL_TEXTURE0);
+	void Bind(GLenum textureUnit = GL_TEXTURE0, GLenum access = GL_READ_ONLY);
 
 	GLuint m_Texture = 0;
-
+	GLenum m_Format = 0;
 	enum TextureType : GLint { cInvalid = 0, cDDS = 1, cPNG = 2};
-	TextureType m_Type;
+	TextureType m_Type;	
 };
 
 #endif 
